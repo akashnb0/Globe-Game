914ecba739bbd36e2ae02aed203874bddd4b1dfd
Akash S <108513566+akashnb0@users.noreply.github.com>
2024-09-01 16:04:36 +0530

    Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,26 +13,12 @@ export default function Footer() {
   return (
     <footer className="pt-8 pb-4 text-xs flex items-end justify-between w-full">
       <span className="max-w-[40%] space-x-1">
-        <a href="https://the-abe-train.com">
+        <a href="https://akashs.vercel.app">
           <FormattedMessage id="Footer1" />
         </a>
+       
         <a
-          href="https://twitter.com/theAbeTrain"
-          aria-label="Twitter"
-          className="inline"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width={iconWidth}
-            viewBox="0 0 24 24"
-            fill={nightMode ? "rgb(209 213 219)" : "rgb(17 24 39)"}
-            className="inline"
-          >
-            <path d={getPath("twitter")} />
-          </svg>
-        </a>
-        <a
-          href="https://github.com/the-abe-train"
+          href="https://github.com/akashnb0"
           aria-label="Github"
           className="inline"
         >
